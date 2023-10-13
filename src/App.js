d62b08a4fc08f8de77bd086e80171cdb236c3fd3
HPOIZAT <hugo@LAPTOP-Q2U79OEJ.localdomain>
2023-10-13 10:06:44 +0200

    Centrage bouton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import {FaHome, FaInfo, FaShoppingBasket} from 'react-icons/fa';
+import {FaHome, FaInfo, FaShoppingBasket, FaUser} from 'react-icons/fa';
 import './App.css';
 
 //Organisms
 import About from './components/organisms/About/About';
 import Header from './components/organisms/Header/Header';
 import Home from './components/organisms/Home/Home';
+import Profile from './components/organisms/Profile/Profile';
 import Shop from './components/organisms/Shop/Shop';
 
 //Internal Imports
@@ -16,39 +17,41 @@ function App() {
 
   const menu = [
     {
-        label: "Home",
-        icon: <FaHome />,
-        action: () => {
-          console.log("Home clicked")
-          setActivePage("Home")
-        }
+      label: "Home",
+      icon: <FaHome />,
+      action: () => {
+        console.log("Home clicked")
+        setActivePage("Home")
+      }
     },
     {
-        label: "Shop",
-        icon: <FaShoppingBasket />,
-        action: () => {
-          console.log("Shop clicked")
-          setActivePage("Shop")
-        }
+      label: "Shop",
+      icon: <FaShoppingBasket />,
+      action: () => {
+        console.log("Shop clicked")
+        setActivePage("Shop")
+      }
     },
     {
-        label: "About",
-        icon: <FaInfo />,
-        action: () => {
-          console.log("About clicked")
-          setActivePage("About")
-        }
+      label: "About",
+      icon: <FaInfo />,
+      action: () => {
+        console.log("About clicked")
+        setActivePage("About")
+      }
     }
   ]
   return (
     <div className={Style.App}>
-      <Header menu={menu} />
+      <Header menu={menu} activePage={activePage} setActivePage={setActivePage} />
       {(() => {
         switch (activePage) {
           case "Shop":
             return <Shop />
           case "About":
             return <About />
+          case "Profile":
+            return <Profile />
           default:
             return <Home />
         } 
