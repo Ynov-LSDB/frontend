5a05bca81bbbbbad1a9c514061580cde83d7863b
guilhemtrd <guilhem.trd@gmail.com>
2023-10-31 16:28:31 +0100

    - Animation logo header

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -14,7 +14,7 @@ import Logo from "../../../assets/images/logo.png";
 const Header = ({menu, activePage, setActivePage}) => {
     return (
         <header className={Style.header}>
-            <img src={Logo} alt="Logo" className={Style.logo} />
+            <img src={Logo}  alt="Logo" className={Style.logo}  />
             <div className={Style.buttons}>
                 {menu.map((elt, index) =>
                     <Button buttonTitle={elt.label} action={elt.action} icon={elt.icon} activePage={activePage} key={index} />
