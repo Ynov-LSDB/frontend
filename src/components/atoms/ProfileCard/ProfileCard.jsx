fb0be981a9cead14c68ce6ed1e11077c0931e56f
guilhemtrd <guilhem.trd@gmail.com>
2023-11-04 17:07:40 +0100

    Ajout de tailwind

--- src/components/atoms/ProfileCard/ProfileCard.jsx
+++ src/components/atoms/ProfileCard/ProfileCard.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
-//Internal imports
-import styles from './ProfileCard.module.css';
-
-
-const ProfileCard = ({ style, children }) => (
-    <div className={styles.card} style={style}>
+const ProfileCard = ({ children }) => (
+    <div className="bg-white shadow rounded-lg overflow-hidden">
         {children}
     </div>
 );
--- src/components/molecules/ProfileCards/LeftProfileCard/LeftProfileCard.jsx
+++ src/components/molecules/ProfileCards/LeftProfileCard/LeftProfileCard.jsx
@@ -1,24 +1,21 @@
 import React from "react";
-
-// Components imports
 import Card from "../../../atoms/ProfileCard/ProfileCard";
 import Text from '../../../atoms/ProfileText/Text';
 
-// Internal imports
-import styles from './LeftProfileCard.module.css';
-
 const LeftProfileCard = ({ user }) => (
-    <div className={styles.container}>
-        <Card style={{ width: '100%', height: `calc(45% + 45% + 20px)` }}>
-            <div className={styles.image} style={{backgroundImage: `url(${user.imageURL || 'https://www.w3schools.com/howto/img_avatar.png'})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}></div>
-            <div className={styles.textContainer}>
-                <Text content={user.firstName} />
-                <Text content={user.lastName} />
-                <Text content={user.birthDate} />
+    <div className="w-1/3 p-4 flex flex-col items-center">
+        <Card>
+            <div
+                className="w-full bg-no-repeat bg-cover bg-center rounded-t-lg h-56"
+                style={{ backgroundImage: `url(${user.imageURL || 'https://www.w3schools.com/howto/img_avatar.png'})` }}
+            ></div>
+            <div className="p-4 text-center">
+                <Text content={user.firstName} customStyles="text-lg font-semibold" />
+                <Text content={user.lastName} customStyles="text-lg" />
+                <Text content={user.birthDate} customStyles="text-sm text-gray-600" />
             </div>
         </Card>
     </div>
 );
 
 export default LeftProfileCard;
-
