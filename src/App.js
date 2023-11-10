64b9ae533d1e70e5895a4dd1aadc4f422ff59764
Nvmeless <59935640+Nvmeless@users.noreply.github.com>
2023-11-10 15:57:28 +0100

    Add Routeur implementation /!\ no imported

--- src/App.js
+++ src/App.js
@@ -15,11 +15,16 @@ import backgroundImage from "./assets/images/bg.jpg";
 
 function App() {
   const [activePage, setActivePage] = useState("Home")
+  //Integrer React Routeur ici
 
+
+
+  
   const menu = [
     {
       label: "Home",
-      icon: <FaHome />,
+      // icon: (<Link to='/'><FaHome /></Link>),
+      icon: (<FaHome />),
       action: () => {
         console.log("Home clicked")
         setActivePage("Home")
@@ -27,7 +32,8 @@ function App() {
     },
     {
       label: "Events",
-      icon: <FaCalendar />,
+      // icon: (<Link to='/events'><FaCalendar /></Link>),
+      icon: (<FaCalendar />),
       action: () => {
         console.log("Events clicked")
         setActivePage("Events")
@@ -35,7 +41,8 @@ function App() {
     },
     {
       label: "About",
-      icon: <FaInfo />,
+      // icon: (<Link to='/events'> <FaInfo /></Link>),
+      icon: (<FaInfo />),
       action: () => {
         console.log("About clicked")
         setActivePage("About")
@@ -43,9 +50,22 @@ function App() {
     }
   ]
   return (
+    //<Router>
     <div className={Style.App}>
       <Header menu={menu} activePage={activePage} setActivePage={setActivePage} className={Style.Header} />
       <img src={backgroundImage} class="backgroundImage"/>
+      /*
+    <Header menu={menu} ... />
+    <img src={backgroundImage} class="backgroundImage"/>
+    </Routeur>
+    <Routes>
+      <Route path="/" element={<Home />}>
+      <Route path="/about" element={<About />}>
+      <Route path="/profile" element={<Profile />}>
+      <Route path="/events" element={<Events />}>
+    </Routes>
+  */
+
       {(() => {
         switch (activePage) {
           case "Events":
@@ -59,6 +79,7 @@ function App() {
         } 
       })()}
     </div>
+//    </Router>
   );
 }
 
