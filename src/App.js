05acf5924f5e4bc2f3b5a5852a3a6841287bafe8
MatteoDinville <matteo.dinville@ynov.com>
2023-11-10 16:39:19 +0100

    Add Routeur implementation /!\ no imported

--- src/App.js
+++ src/App.js
@@ -10,19 +10,72 @@ import Login from './components/organisms/Auth/Login';
 import Register from './components/organisms/Auth/Register';
 
 function App() {
+  const [activePage, setActivePage] = useState("Home")
+  //Integrer React Routeur ici
+
+
+
+  
+  const menu = [
+    {
+      label: "Home",
+      // icon: (<Link to='/'><FaHome /></Link>),
+      icon: (<FaHome />),
+      action: () => {
+        console.log("Home clicked")
+        setActivePage("Home")
+      }
+    },
+    {
+      label: "Events",
+      // icon: (<Link to='/events'><FaCalendar /></Link>),
+      icon: (<FaCalendar />),
+      action: () => {
+        console.log("Events clicked")
+        setActivePage("Events")
+      }
+    },
+    {
+      label: "About",
+      // icon: (<Link to='/events'> <FaInfo /></Link>),
+      icon: (<FaInfo />),
+      action: () => {
+        console.log("About clicked")
+        setActivePage("About")
+      }
+    }
+  ]
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/userProfile" component={UserProfile} />
-          <Route path="/events" component={Events} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Routes>
-      </div>
-    </Router>
+    //<Router>
+    <div className={Style.App}>
+      <Header menu={menu} activePage={activePage} setActivePage={setActivePage} className={Style.Header} />
+      <img src={backgroundImage} class="backgroundImage"/>
+      /*
+    <Header menu={menu} ... />
+    <img src={backgroundImage} class="backgroundImage"/>
+    </Routeur>
+    <Routes>
+      <Route path="/" element={<Home />}>
+      <Route path="/about" element={<About />}>
+      <Route path="/profile" element={<Profile />}>
+      <Route path="/events" element={<Events />}>
+    </Routes>
+  */
+
+      {(() => {
+        switch (activePage) {
+          case "Events":
+            return <Events />
+          case "About":
+            return <About />
+          case "Profile":
+            return <Profile />
+          default:
+            return <Home />
+        } 
+      })()}
+    </div>
+//    </Router>
   );
 }
 
