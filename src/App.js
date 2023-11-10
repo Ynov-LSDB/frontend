2a5def73b04cc7617b4fe15c0ef1e1a99befefff
MatteoDinville <matteo.dinville@ynov.com>
2023-11-10 15:42:43 +0100

    Try to create router navigation with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,84 +1,28 @@
-import React, { useState } from 'react';
-import {FaCalendar, FaHome, FaInfo, FaLock, FaUnLock, FaUser} from 'react-icons/fa';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
-//Organisms
-import About from './components/templates/About/About';
 import Header from './components/organisms/Header/Header';
 import Home from './components/templates/Home/Home';
-import Profile from './components/templates/Profile/Profile';
-import Events from "./components/templates/Events/Events";
+import UserProfile from './components/organisms/UserProfile';
+import Events from './components/templates/Events/Events';
 import Login from './components/organisms/Auth/Login';
 import Register from './components/organisms/Auth/Register';
-//Internal Imports
-import Style from './App.css';
-import backgroundImage from "./assets/images/bg.jpg";
 
 function App() {
-  const [activePage, setActivePage] = useState("Home")
-
-  const menu = [
-    {
-      label: "Home",
-      icon: <FaHome />,
-      action: () => {
-        console.log("Home clicked")
-        setActivePage("Home")
-      }
-    },
-    {
-      label: "Events",
-      icon: <FaCalendar />,
-      action: () => {
-        console.log("Events clicked")
-        setActivePage("Events")
-      }
-    },
-    {
-      label: "About",
-      icon: <FaInfo />,
-      action: () => {
-        console.log("About clicked")
-        setActivePage("About")
-      }
-    },
-    {
-      label: "Login",
-      icon: <FaLock />,
-      action: () => {
-        console.log("Shop clicked")
-        setActivePage("Login")
-      }
-    },
-    {
-      label: "Register",
-      icon: <FaLock />,
-      action: () => {
-        console.log("Shop clicked")
-        setActivePage("Register")
-      }
-    }
-  ]
   return (
-    <div className={Style.App}>
-      <Header menu={menu} activePage={activePage} setActivePage={setActivePage} className={Style.Header} />
-      {(() => {
-        switch (activePage) {
-          case "Events":
-            return <Events />
-          case "About":
-            return <About />
-          case "Profile":
-            return <Profile />
-          case "Login":
-            return <Login />
-          case "Register":
-          return <Register />
-          default:
-            return <Home />
-        } 
-      })()}
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/userProfile" component={UserProfile} />
+          <Route path="/events" component={Events} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,86 +1,57 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { FaBars, FaUser, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-// Atoms
-import Button from "../../atoms/Button/Button";
+import Style from './Header.module.css';
+import Logo from '../../../assets/images/logo.png';
 
-// Logo
-import Logo from "../../../assets/images/logo.png";
-
-import Style from "./Header.module.css";
-
-const Header = ({ menu, activePage, setActivePage }) => {
-    // State to handle the visibility of the burger menu
+const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    // Function to toggle the menu
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
-    // Function to handle menu item click
-    const handleMenuItemClick = (label) => {
-        setActivePage(label);
-        setIsMenuOpen(false);
-    };
-
     return (
         <header className={`bg-gray-800 h-16 flex items-center p-2 relative ${Style.header}`}>
-            <img src={Logo} alt="Logo" className={Style.logo} />
+            <Link to="/">
+                <img src={Logo} alt="Logo" className={Style.logo} />
+            </Link>
 
-            {/* Desktop Menu */}
-            <div className="hidden md:flex flex-1 justify-center">
-                {menu.map((elt, index) => (
-                    <Button
-                        buttonTitle={elt.label}
-                        action={() => handleMenuItemClick(elt.label)}
-                        icon={elt.icon}
-                        activePage={activePage}
-                        key={index}
-                    />
-                ))}
+            <div className="hidden md:flex flex-1 justify-center justify-around">
+                <Link to="/profile" className={Style.menuLink}>
+                    Profile
+                </Link>
+                <Link to="/events" className={Style.menuLink}>
+                    Events
+                </Link>
             </div>
 
-            {/* Profile Button */}
             <div className="hidden md:block mr-5">
-                <Button
-                    buttonTitle="Profile"
-                    action={() => handleMenuItemClick("Profile")}
-                    icon={<FaUser />}
-                    activePage={activePage}
-                    key="profile"
-                />
+                <Link to="/userProfile" className={Style.menuLink}>
+                    Profile
+                </Link>
             </div>
 
-            {/* Burger Menu Icon */}
             <div className="md:hidden ml-5">
                 <button onClick={toggleMenu} className={`text-white ${Style.menuButton}`}>
                     {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
                 </button>
             </div>
 
-            {/* Mobile Menu: Shown/Hidden based on state */}
             {isMenuOpen && (
                 <div className={`absolute top-full right-0 w-full bg-gray-800 flex flex-col items-center py-3 z-1${Style.mobileMenu}`}>
-                    {menu.map((elt, index) => (
-                        <Button
-                            buttonTitle={elt.label}
-                            action={() => handleMenuItemClick(elt.label)}
-                            icon={elt.icon}
-                            activePage={activePage}
-                            key={`mobile-${index}`}
-                        />
-                    ))}
-                    <Button
-                        buttonTitle="Profile"
-                        action={() => handleMenuItemClick("Profile")}
-                        icon={<FaUser />}
-                        activePage={activePage}
-                        key="mobile-profile"
-                    />
+                    <Link to="/" className={Style.menuLink}>
+                        Home
+                    </Link>
+                    <Link to="/userProfile" className={Style.menuLink}>
+                        Profile
+                    </Link>
+                    <Link to="/events" className={Style.menuLink}>
+                        Events
+                    </Link>
                 </div>
             )}
-
         </header>
     );
 };
