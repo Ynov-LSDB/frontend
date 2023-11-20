a871c70c067e42a27dde40b7a450958f935fd697
HPOIZAT <hugo@LAPTOP-Q2U79OEJ.localdomain>
2023-11-20 15:17:44 +0100

    :zap: Ajout récupération de dernier evenement avec le back

--- src/components/atoms/Home/HomeLeftCard.jsx
+++ src/components/atoms/Home/HomeLeftCard.jsx
@@ -1,34 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { FaUserGroup, FaLocationDot, FaMoneyBillWave } from "react-icons/fa6";
+import { FaCalendarDay } from "react-icons/fa";
 
-const nextEvent = {
-    "id": 1,
-    "title": "Pétanque au bourg !",
-    "date": "2023-12-05T20:00:00",
-    "price": "25€",
-    "category_id": 3,
-    "adresse": "Place de la République, Paris",
-    "eat": "Pizza et boissons disponibles",
-    "registered_limit": "300 personnes",
-    "status": "Ouvert",
-    "team_style": "Triplette",
-    "created_at": "2023-11-01T14:00:00",
-    "updated_at": "2023-11-02T12:30:00"
-}
 const HomeLeftCard = () => {
+    const [nextEvent, setEvent] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(2000)
+        axios.get('http://localhost:80/api/event/last')
+            .then(response => {
+                if (response.data.success) {
+                    setEvent(response.data.data);
+                } else {
+                    console.error('Data not found');
+                }
+            }).catch(error => {
+                console.error('Error fetching data:', error);
+            });
+
+        setLoading(false);
+    }, []);
+
     return (
         <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col">
-            <h2 className="text-xl font-bold mb-4">Prochain événement</h2>
-            <p className="text-lg font-semibold mb-2">{nextEvent.title}</p>
-            <p className="text-sm text-gray-600 mb-1"><strong>Date :</strong> {new Date(nextEvent.date).toLocaleDateString("fr-FR", { year: 'numeric', month: 'long', day: 'numeric' })} à {new Date(nextEvent.date).toLocaleTimeString("fr-FR", { hour: '2-digit', minute: '2-digit' })}H</p>
-            <p className="text-sm text-gray-600 mb-1"><strong>Prix :</strong> {nextEvent.price}</p>
-            <p className="text-sm text-gray-600 mb-1"><strong>Adresse :</strong> {nextEvent.adresse}</p>
-            <p className="text-sm text-gray-600 mb-1"><strong>Nourriture :</strong> {nextEvent.eat}</p>
-            <p className="text-sm text-gray-600 mb-1"><strong>Limitation :</strong> {nextEvent.registered_limit}</p>
-            <p className="text-sm text-gray-600 mb-1"><strong>Statut :</strong> {nextEvent.status}</p>
-            <p className="text-sm text-gray-600 mb-1"><strong>Style d'équipe :</strong> {nextEvent.team_style}</p>
-            <div className="flex-grow"></div>
+            <h2 className="text-xl font-bold mb-4 text-center">Dernier évènement ajouté</h2>
+            {loading ? 
+                <div className="flex justify-center items-center h-screen">
+                    <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-black"></div>
+                </div>
+                :
+                nextEvent !== undefined && nextEvent !== null ? 
+                    <div className="text-sm text-gray-600">
+                        <p className="text-lg font-semibold mb-2">{nextEvent.title}</p>
+                        <p className="mb-1 flex"> <FaCalendarDay className="mx-1" size={20} /> {new Date(nextEvent.date).toLocaleDateString("fr-FR", { year: 'numeric', month: 'long', day: 'numeric' })} à {new Date(nextEvent.date).toLocaleTimeString("fr-FR", { hour: '2-digit', minute: '2-digit' })}</p>
+                        <p className="mb-1 flex"> <FaLocationDot className="mx-1" size={20} /> {nextEvent.adresse}</p>
+                        <p className="mb-1 flex"> <FaMoneyBillWave className="mx-1" size={20} /> {nextEvent.price}€</p>
+                        <p className="mb-1 flex"> <FaUserGroup className="mx-1" size={20} /> {nextEvent.registered_limit}</p>
+                        <p className="mb-1 flex"> {nextEvent.team_style} </p>
+                        <div className="flex-grow"></div>
+                        <button className="bg-transparent border border-gray-600 font-bold mt-4 py-2 px-4 rounded-full hover:bg-gray-200">
+                            Voir plus
+                        </button>
+                    </div>
+                    :
+                    <div>
+                        <h2 className="text-xl font-bold mb-4">Aucun évènement n'a été trouvé.</h2>
+                    </div>
+            }   
         </div>
     );
 };
+
+
 
 export default HomeLeftCard;
