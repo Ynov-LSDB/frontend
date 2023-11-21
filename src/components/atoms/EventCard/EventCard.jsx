1a6d656d5e13616698f047790b5a4e7d5b8050f1
QuentinSAIL <saillardq@gmail.com>
2023-11-21 18:11:30 +0100

    :zap: display userInEvent & notIn

--- src/components/atoms/EventCard/EventCard.jsx
+++ src/components/atoms/EventCard/EventCard.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-const EventCard = ({ title, contenu, image, alt = title }) => {
+const EventCard = ({ title, description, image, canJoin = true }) => {
     const [showPopup, setShowPopup] = useState(false);
 
     const toggleShowPopup = () => {
@@ -8,24 +8,26 @@ const EventCard = ({ title, contenu, image, alt = title }) => {
 
     return (
         <div className="shadow-lg text-center p-2.5 bg-gray-800 text-white m-2.5 w-[500px] rounded-md">
-            <img src={image} alt={alt} className="w-full h-[300px] object-cover" />
+            <img src={image} className="w-full h-[300px] object-cover" />
             <h1 className="text-xl font-bold">{title}</h1>
-            <p>{contenu.length > 140 ? contenu.slice(0, 140) + "..." : contenu}</p>
+            <p>{description.length > 140 ? description.slice(0, 180) + "..." : description}</p>
             {showPopup && (
                 <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center" onClick={toggleShowPopup}>
                     <div className="bg-white p-5 rounded-lg shadow-xl text-center text-black w-1/2">
-                        <img src={image} alt={alt} className="w-full h-auto" />
+                        <img src={image} className="w-full h-auto" />
                         <h1 className="text-xl font-bold">{title}</h1>
-                        <p>{contenu}</p>
+                        <p>{description}</p>
                     </div>
                 </div>
             )}
-            <button onClick={toggleShowPopup} className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-3 px-5 rounded">
+            <button onClick={toggleShowPopup} className="bg-gray hover:bg-gray-700 text-white font-bold py-3 px-5 rounded">
                 En savoir plus
             </button>
-            <button className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-3 px-5 rounded">
-                    Rejoindre un événement
+            {canJoin && (
+                <button className="bg-gray hover:bg-gray-700 text-white font-bold py-3 px-5 rounded">
+                    Rejoindre l'événement
                 </button>
+            )}
         </div>
     );
 };
--- src/components/templates/Events/Events.jsx
+++ src/components/templates/Events/Events.jsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import api from "../../../toolkit/api.config";
 import EventCard from "../../atoms/EventCard/EventCard";
+import ConcoursBase from "../../../assets/images/concours-base.jpg";
 
 const Events = () => {
+    const [events, setEvents] = useState(null);
+    const [userisIn, setUserisIn] = useState(true);
 
-    const events = [
-        {
-            title: "yeah",
-            contenu: "Grosse compétition de fameux bouliste ce mardi en 8 sortez vos boules il va pleuvoir du plomb",
-            image: "https://i.ytimg.com/vi/LRyTWCzqEDk/maxresdefault.jpg"
-        },
-        {
-            title: "chraik",
-            contenu: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu tristique felis, in auctor elit. Duis rhoncus ornare sodales. Vestibulum tincidunt nulla quis felis lobortis volutpat. Vivamus a ex vel odio pretium aliquet. Sed posuere luctus elit id egestas. Morbi sit amet ligula mi. Etiam tempor fringilla nunc, sit amet ultricies nulla tincidunt quis. Vestibulum id orci lacus. Nam blandit tincidunt felis et pretium. Nulla facilisi.",
-            image: "https://www.ecranlarge.com/media/cache/1600x1200/uploads/articles/001/375/032/shrek-classement-shrek-et-chat-potte-1471970-large.png"
-        },
-        {
-            title: "apero salle des fete",
-            contenu: "pointeur et tireur, tout le monde est le bienvenue pour un apero salle des fete",
-            image: "https://image.jimcdn.com/app/cms/image/transf/none/path/s47054cd4448bf150/image/id772b6c61b145509/version/1448724461/image.jpg"
+    const fetchData = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            const response = await axios(api("get", userisIn ? "user/inEvent" : "user/notInEvent", null, token));
+
+            if (response.data.success) {
+                console.log(response.data.data)
+                setEvents(response.data.data);
+            } else {
+                console.error("Error:", response.data.message);
+            }
+        } catch (error) {
+            console.error("Error fetching Event data:", error);
         }
-    ];
+    };
+
+    useEffect(() => {
+        fetchData().then(() => console.log("Events fetched"));
+    }, []); // for initial mount
+
+    useEffect(() => {
+        fetchData().then(() => console.log("Events fetched"));
+    }, [userisIn]);
 
     return (
         <div className="flex flex-col items-center bg-opacity-80 p-5">
@@ -27,23 +38,26 @@ const Events = () => {
                 <button className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-3 px-5 rounded">
                     Créer un événement
                 </button>
-                <button className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-3 px-5 rounded">
-                    Mes événements
+                <button
+                    className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-3 px-5 rounded"
+                    onClick={() => setUserisIn(!userisIn)}
+                >
+                    {userisIn ? "Rejoindre des évenements" : "Mes évenements"}
                 </button>
             </div>
             <div className="flex flex-wrap justify-around w-4/5 mt-4">
-                {events.map((event, index) => (
+                {events ? events.map((event, index) => (
                     <EventCard
                         key={index}
                         title={event.title}
-                        contenu={event.contenu}
-                        image={event.image}
+                        description={event.description}
+                        image={event.imageURL || ConcoursBase}
+                        canJoin={!userisIn}
                     />
-                ))}
+                )): <div className="text-2xl">Aucun événement</div> }
             </div>
         </div>
     );
-    
-}
+};
 
 export default Events;
