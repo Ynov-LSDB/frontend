5484f8912e84fb5fb9401390f946d315d8ce239a
HPOIZAT <hugo@LAPTOP-Q2U79OEJ.localdomain>
2023-11-23 14:03:22 +0100

    :zap: Style custom classement

--- src/components/templates/Ranking/Ranking.jsx
+++ src/components/templates/Ranking/Ranking.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import {
-    createColumnHelper,
-    flexRender,
     getCoreRowModel,
     useReactTable,
 } from '@tanstack/react-table';
@@ -13,20 +11,6 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 // Atoms
 import Loader from "../../atoms/Loader";
-  
-const columnHelper = createColumnHelper();
-
-const columns = [
-    columnHelper.accessor('rank', {
-        header: 'Rang',
-    }),
-    columnHelper.accessor('name', {
-        header: 'Nom',
-    }),
-    columnHelper.accessor('score', {
-        header: 'Score',
-    }),
-];
 
 const Ranking = () => {
 
@@ -57,7 +41,6 @@ const Ranking = () => {
 
     const table = useReactTable({
         data: data.data || [],
-        columns,
         state: {
           pagination,
         },
@@ -71,62 +54,87 @@ const Ranking = () => {
         <div className="flex justify-center">
             {loading 
                 ? (<Loader />)
-                :<div className="relative overflow-x-auto shadow-md sm:rounded-lg my-5 w-3/4">
+                :<div className="relative overflow-x-auto shadow-md sm:rounded-lg my-5 w-3/5">
                     <table className="w-full text-sm text-gray-500">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 h-10 border-b-4 text-center">
-                            {table.getHeaderGroups().map((headerGroup) => (
-                                <tr className="px-6 py-3">
-                                    {headerGroup.headers.map((header) => (
-                                        <th>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                    header.column.columnDef.header,
-                                                    header.getContext()
-                                            )}
-                                        </th>
-                                    ))}
-                                </tr>
-                            ))}
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 h-12 border-b-4 text-center">
+                            <tr className="px-6 py-3 text-base">
+                                <th> Rang </th>
+                                <th> Nom </th>
+                                <th> Score </th>
+                            </tr>
                         </thead>
                         <tbody>
                             { table.getRowModel().rows.map((row) => (
                                 <tr className="border-b border-gray-200">
-                                    {row.getVisibleCells().map((cell) => (
-                                        <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50">
-                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                        </th>
-                                    ))}
+                                    <th className="font-medium text-gray-900 whitespace-nowrap bg-gray-50 py-2 flex justify-center">
+                                        <div className={getRankStyle(row.original.rank)}>
+                                            {row.original.rank}
+                                        </div>
+                                    </th>
+                                    <th className="font-medium text-gray-900 whitespace-nowrap bg-gray-50">
+                                        {row.original.name}
+                                    </th>
+                                    <th className="font-medium text-gray-900 whitespace-nowrap bg-gray-50">
+                                        {row.original.score}
+                                    </th>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
-                        <div className="w-full text-xs text-gray-700 uppercase bg-gray-50 border-t-4 h-14 flex justify-center items-center content-center">
-                        <button
-                            disabled={!table.getCanPreviousPage()}
-                            onClick={() => table.previousPage()}
-                            color="primary"
-                            variant="contained"
-                        >
-                            <FaChevronLeft size={20} />
-                        </button>
-                        <span className="text-lg mx-3">
-                            {table.getState().pagination.pageIndex + 1} /{' '}
-                            {table.getPageCount()}
+                    <div className="w-full text-xs text-gray-700 uppercase bg-gray-50 border-t-4 h-12 flex items-center">
+                        <div className="w-1/3"> {' '} </div>
+                        <div className="w-1/3 flex justify-center">
+                            <button
+                                disabled={!table.getCanPreviousPage()}
+                                onClick={() => table.previousPage()}
+                                color="primary"
+                                variant="contained"
+                            >
+                                <FaChevronLeft size={20} />
+                            </button>
+                            <span className="text-lg mx-3">
+                                {table.getState().pagination.pageIndex + 1} /{' '}
+                                {table.getPageCount()}
+                            </span>
+                            <button
+                                disabled={!table.getCanNextPage()}
+                                onClick={() => table.nextPage()}
+                                color="primary"
+                                variant="contained"
+                            >
+                                <FaChevronRight size={20} />
+                            </button>
+                        </div>
+                        <span className="w-1/3 flex justify-end pr-5 items-center">
+                            Aller à la page :
+                            <input
+                                type="number"
+                                defaultValue={table.getState().pagination.pageIndex + 1}
+                                onChange={e => {
+                                    const page = e.target.value ? Number(e.target.value) - 1 : 0
+                                    table.setPageIndex(page)
+                                }}
+                                className="border p-1 rounded w-10 ml-1"
+                            />
                         </span>
-                        <button
-                            disabled={!table.getCanNextPage()}
-                            onClick={() => table.nextPage()}
-                            color="primary"
-                            variant="contained"
-                        >
-                            <FaChevronRight size={20} />
-                        </button>
                     </div>
                 </div>
             }
         </div> 
     );
 };
+
+function getRankStyle(rank) {
+    switch (rank) {
+        case 1:
+            return "bg-yellow-400 rounded-full w-6 h-6";
+        case 2:
+            return "bg-gray-400 rounded-full w-6 h-6";
+        case 3:
+            return "bg-yellow-600 rounded-full w-6 h-6";
+        default:
+            return "";
+    }
+}
 
 export default Ranking;
